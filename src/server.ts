0d3f6b3d080c5966443693fcda378b94ca13153a
rbpolim <rodrigobpolim@gmail.com>
2021-04-30 09:37:24 -0300

    Chapter-3-usuaruis

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import "reflect-metadata";
 
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
+import { AppError } from "./errors/AppError";
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
 
@@ -18,6 +20,22 @@ app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
+
+// Middleware para tratativas de Erros
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: "error",
+      message: `Internal server error - ${err.message}`,
+    });
+  }
+);
 
 app.listen(3333, () => {
   console.log("🚀 Server started on port 3333");
